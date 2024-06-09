a1dce8dd3b430ee859d3a3dae2b4d02a64e3c0d3
wrimsand <wrimsand@gmail.com>
2024-06-09 16:41:51 -0400

    Initial Commit

---
+++ youtube-onedaybuild/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  youtube-onedaybuild
+//
+//  Created by Reagan Imsand on 6/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ youtube-onedaybuild/youtube_onedaybuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_onedaybuildApp.swift
+//  youtube-onedaybuild
+//
+//  Created by Reagan Imsand on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct youtube_onedaybuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
