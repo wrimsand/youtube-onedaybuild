9ececeab3d4ca56f2cd79137b1c108e008ced32b
wrimsand <wrimsand@gmail.com>
2024-06-09 17:44:20 -0400

    Test commit

--- youtube-onedaybuild/ContentView.swift
+++ youtube-onedaybuild/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    var temp : Int = 0
     var body: some View {
         VStack {
             Image(systemName: "globe")
--- youtube-onedaybuild/youtube_onedaybuildApp.swift
+++ youtube-onedaybuild/youtube_onedaybuildApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct youtube_onedaybuildApp: App {
+
     var body: some Scene {
         WindowGroup {
             ContentView()
